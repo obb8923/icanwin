4070e6f51fd96b9803d9006e61e7941a984fe422
obb8923 <obb8923@naver.com>
2025-07-15 15:09:13 +0900

    .

--- src/domain/fight/pages/FightPage.tsx
+++ src/domain/fight/pages/FightPage.tsx
@@ -77,7 +77,7 @@ export const FightPage = () => {
            
         </div>
         {/* 버튼 영역 */}
-        {!AIResult && (
+        {AIResult && (
           <div className="w-full max-w-lg md:max-w-2xl lg:max-w-xl flex justify-between items-center gap-4">
             {/* <button
               className="flex-1  mt-2 px-4 py-2 bg-white text-gray-900 rounded-xl hover:bg-gray-300 transition-colors"
