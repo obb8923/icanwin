c1ba85d0ab426c1fed46ded47dd354677d0e02ec
obb8923 <obb8923@naver.com>
2025-07-15 12:41:31 +0900

    bng

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FightPage } from "./domain/fight/pages/FightPage";
-import BottomNavigation from "./shared/components/BottomNavigation";
+import {BottomNavigation} from "./shared/components/BottomNavigation";
 import './App.css'
 function App() {
   return (
